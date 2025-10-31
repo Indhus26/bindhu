309e0316cdc4e0bb9f9a3a46fd88f58325e0f102
Indhus26 <127784110+Indhus26@users.noreply.github.com>
2025-10-31 15:51:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "random_id" "bucket_id" {
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1"
 
   tags = {
     Name = "Terraform-public-subnet"
